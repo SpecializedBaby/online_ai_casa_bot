b870a0d67c46114e43c9ffe525a1d10f8cdc2051
Dede <specialized8393@gmail.com>
2025-07-19 18:42:29 +0100

    create crypto payment method by CryptoBot merch via aiocryptopay package -v 0.4.8
    Methods:
    - get_info_crypto_app
    - create_invoice
    - create_fiat_invoice
    - get_old_invoice
    - get_invoice_id
    - delete_invoice

---
+++ bot/services/crypto.py
@@ -0,0 +1,62 @@
+import os
+
+from aiocryptopay import AioCryptoPay, Networks
+from aiocryptopay.const import InvoiceStatus
+from aiocryptopay.models.invoice import Invoice
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+crypto = AioCryptoPay(token=os.getenv("CRYPTO_PAY_TOKEN"), network=Networks.TEST_NET)  # Networks.MAIN_NET on prod action
+
+
+async def get_info_crypto_app():
+    profile = await crypto.get_me()
+    currencies = await crypto.get_currencies()
+    balance = await crypto.get_balance()
+    rates = await crypto.get_exchange_rates()
+    stats = await crypto.get_stats()
+
+    return {
+        "profile": profile,
+        "currencies": currencies,
+        "balance": balance,
+        "rates": rates,
+        "stats": stats,
+    }
+
+
+async def create_invoice(amount: float, currency: str = "USDT") -> Invoice:
+    invoice = await crypto.create_invoice(asset='USDT', amount=amount)
+    print(f"URL invoice: {invoice.bot_invoice_url}")
+    return invoice
+
+
+async def create_fiat_invoice(
+        amount: float,
+        fiat: str = "USD",
+        currency_type: str = "fiat"
+) -> Invoice:
+    fiat_invoice = await crypto.create_invoice(
+        amount=amount,
+        fiat=fiat,
+        currency_type=currency_type
+    )
+    print(fiat_invoice)
+    return fiat_invoice
+
+
+async def get_old_invoice(invoice_id: int) -> InvoiceStatus | str:
+    old_invoice = await crypto.get_invoices(invoice_ids=invoice_id)
+    print(old_invoice.status)
+    return old_invoice.status
+
+
+def get_invoice_id(invoice: Invoice) -> int:
+    return invoice.invoice_id
+
+
+async def delete_invoice(invoice_id: int) -> bool:
+    deleted_invoice = await crypto.delete_invoice(invoice_id=invoice_id)
+    return deleted_invoice
